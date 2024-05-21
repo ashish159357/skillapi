b7106e4c73031c394baa64b048e5cc3d22bd0200
ashish <ashishsingh159258@gmail.com>
2024-05-21 18:57:53 +0530

    eureka add

--- src/main/java/com/techhitter/main.java
+++ src/main/java/com/techhitter/main.java
@@ -2,17 +2,16 @@ package com.techhitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		ApplicationContext con = SpringApplication.run(main.class,args);
-		
-
 	}
 
 }
